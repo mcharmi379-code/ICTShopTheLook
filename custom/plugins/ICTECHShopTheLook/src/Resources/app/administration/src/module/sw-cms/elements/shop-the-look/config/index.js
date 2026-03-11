50f9df0c577b847395ed0f33a771a85502258e76
Charmi Mistry <charmi@icreativetechnologies.com>
2026-03-11 12:35:56 +0530

    fixed the file upload issue

--- custom/plugins/ICTECHShopTheLook/src/Resources/app/administration/src/module/sw-cms/elements/shop-the-look/config/index.js
+++ custom/plugins/ICTECHShopTheLook/src/Resources/app/administration/src/module/sw-cms/elements/shop-the-look/config/index.js
@@ -23,6 +23,18 @@ Component.register('sw-cms-el-config-ict-shop-the-look', {
     },
 
     computed: {
+        mediaRepository() {
+            return this.repositoryFactory.create('media');
+        },
+
+        uploadTag() {
+            return `cms-element-media-config-${this.element.id}`;
+        },
+
+        defaultFolderName() {
+            return this.cmsPageState._entityName;
+        },
+
         productRepository() {
             return this.repositoryFactory.create('product');
         },
@@ -49,6 +61,8 @@ Component.register('sw-cms-el-config-ict-shop-the-look', {
 
     created() {
         this.initElementConfig('ict-shop-the-look');
+        console.log('Shop The Look - Element Created:', this.element);
+        console.log('Shop The Look - Config:', this.element.config);
 
         this.products = new EntityCollection(
             this.productRepository.route,
@@ -57,11 +71,13 @@ Component.register('sw-cms-el-config-ict-shop-the-look', {
         );
 
         if (this.element.config.products.value?.length) {
+            console.log('Shop The Look - Loading Products:', this.element.config.products.value);
             const criteria = new Criteria();
             criteria.setIds(this.element.config.products.value);
 
             this.productRepository.search(criteria, this.productContext).then((result) => {
                 this.products = result;
+                console.log('Shop The Look - Products Loaded:', result);
             });
         }
     },
@@ -69,9 +85,11 @@ Component.register('sw-cms-el-config-ict-shop-the-look', {
     methods: {
 
         setProductCollection(productCollection) {
+            console.log('Shop The Look - Product Collection Changed:', productCollection);
             this.products = productCollection;
 
             this.element.config.products.value = productCollection.getIds();
+            console.log('Shop The Look - Product IDs Saved:', this.element.config.products.value);
 
             this.$emit('element-update', this.element);
         },
@@ -85,14 +103,31 @@ Component.register('sw-cms-el-config-ict-shop-the-look', {
         },
 
         onMediaSelect(selection) {
+            console.log('Shop The Look - Media Selected:', selection);
             this.element.config.lookImage.value = selection[0];
             this.mediaModalOpen = false;
+            console.log('Shop The Look - Look Image Saved:', this.element.config.lookImage.value);
 
             this.$emit('element-update', this.element);
         },
 
+        onMediaUpload(mediaItem) {
+            console.log('Shop The Look - Media Uploaded:', mediaItem);
+            this.setMediaItem({ targetId: mediaItem.targetId });
+        },
+
+        async setMediaItem({ targetId }) {
+            console.log('Shop The Look - Setting Media Item:', targetId);
+            const media = await this.mediaRepository.get(targetId);
+            this.element.config.lookImage.value = media;
+            console.log('Shop The Look - Media Item Set:', media);
+            this.$emit('element-update', this.element);
+        },
+
         onRemoveImage() {
+            console.log('Shop The Look - Image Removed');
             this.element.config.lookImage.value = null;
+            console.log('Shop The Look - Look Image After Remove:', this.element.config.lookImage.value);
 
             this.$emit('element-update', this.element);
         },
@@ -106,10 +141,14 @@ Component.register('sw-cms-el-config-ict-shop-the-look', {
             }
             
             this.element.config.hotspots.value.push({ x, y, productIndex: this.element.config.hotspots.value.length });
+            console.log('Shop The Look - Hotspot Added:', { x, y });
+            console.log('Shop The Look - All Hotspots:', this.element.config.hotspots.value);
             this.$emit('element-update', this.element);
         },
         removeHotspot(index) {
+            console.log('Shop The Look - Removing Hotspot at Index:', index);
             this.element.config.hotspots.value.splice(index, 1);
+            console.log('Shop The Look - Hotspots After Remove:', this.element.config.hotspots.value);
             this.$emit('element-update', this.element);
         }
     }
